bdf93e3cc62030e55899c21a2f6692b566b4b6de
Juan Diego Zapata Granados <61522176+mrjuancho22@users.noreply.github.com>
2023-06-05 19:09:39 -0500

    .

---
+++ main.py
@@ -0,0 +1,54 @@
+import ast
+import pandas as pd
+from fastapi import FastAPI
+from sklearn.neighbors import NearestNeighbors
+
+app = FastAPI()
+
+df = pd.read_csv('Clean_dataset/movies_dataset.csv',index_col=0)
+
+
+
+@app.get("/cantidad_filmaciones_mes/{mes}")
+def cantidad_filmaciones_mes(mes: str):
+    return None
+
+
+
+@app.get("/cantidad_filmaciones_dia/{dia}")
+def cantidad_filmaciones_dia(dia : str):
+    return None
+
+
+
+@app.get("/score_titulo/{titulo_de_la_filmacion}")
+def score_titulo(titulo_de_la_filmacion: str):
+    return None
+
+
+
+@app.get("/votos_titulo/{titulo_de_la_filmacion}")
+def votos_titulo(titulo_de_la_filmacion : str):
+
+    return None
+
+
+
+@app.get("/productoras/{productora}")
+def productoras(productora: str):
+
+    return None
+
+
+
+@app.get("/retorno/{pelicula}")
+def retorno(pelicula : str):
+
+    return None
+
+
+
+@app.get("/recomendacion/{titulo}")
+def recomendacion(titulo:str):
+    
+    return None
