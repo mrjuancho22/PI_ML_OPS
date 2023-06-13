364792e67224d5403cd2efbba9dd44c653d6169d
Juan Diego Zapata Granados <61522176+mrjuancho22@users.noreply.github.com>
2023-06-12 21:00:39 -0500

    .

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import ast
 from fastapi import FastAPI
 from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import StandardScaler
 
 app = FastAPI()
 
@@ -112,4 +114,67 @@ def get_director(nombre_director:str):
 @app.get('/recomendacion/{titulo}')
 def recomendacion(titulo:str):
     '''Ingresas un nombre de pelicula y te recomienda las similares en una lista'''
-    return {'lista recomendada': 0}
+
+    #se crea un clon del df original con las columnas genres, title, vote_average, popularity, para que la funcion recomendacion no consuma demasiados recursos
+    df_b = df_movies.loc[:, ['belongs_to_collection','genres','title','vote_average','popularity']].copy()
+
+    #la columna genres, tiene valores muy complejos, cada fila tiene una lista de diccionarios, con el codigo de abajo, los generos quedan un string sencillo
+    df_b.genres = df_b.genres.apply(lambda x : ast.literal_eval(x))
+    def normalizar_generos(x):
+        #esta funcion, saca los generos de los diccioanrios, volviendolos un string con todos los generos de la pelicula
+        lista =""
+        for i in x:
+            lista += (i['name'])
+            if i == x[-1]:
+                break
+            lista += ","
+        return lista
+    #se aplica el codigo de arriba
+    df_b.genres = df_b.genres.apply(lambda x : normalizar_generos(x)) 
+
+    #al igual que la columna genres, la columna belongs_to_collection tiene datos complejos, con el codigo de abajo se normaliza
+    df_b.belongs_to_collection = df_b.belongs_to_collection.apply(lambda x : ast.literal_eval(x) if type(x) == str else x)
+    df_b.belongs_to_collection = df_b.belongs_to_collection.apply(lambda x : x['name'] if type(x) == dict else x)
+
+    #se crea columnas de valores unicos con cada uno de los valores disponibles de la columna genres
+    generos_df = df_b.genres.str.get_dummies(sep=',')
+
+    #Se crea una columna donde se almacena si los titulos del df son similares al ingresado, con 1 ( si contiene el titulo en su nombre) y con 0 ( si no contiene el titulo en su nombre)
+    df_b['titulo_similar']= df_b.title.apply(lambda x: 1 if titulo in x else 0)
+
+    #Se crea una columna donde se almacena si los titulos del df pertenecen a la misma coleccion que el ingresado,
+    # con 1 ( si contiene el titulo en su nombre) y con 0 ( si no contiene el titulo en su nombre)
+    
+    if df_b.belongs_to_collection.loc[df_b.title == titulo].item() == None:
+        coleccion = 'no pertenece a ninguna coleccion'
+    else:
+        coleccion = df_b.belongs_to_collection.loc[df_b.title == titulo].item()
+    df_b['pertenece_colecion'] = df_b.belongs_to_collection.apply(lambda x: 1 if  coleccion== x else 0)
+
+    #se crea el objeto knn, que será el modelo de recomendación a través de vecinos cercanos
+    knn = NearestNeighbors(n_neighbors=6, algorithm='auto')
+
+    #se normalizan las columnas popularity y vote_average
+    escalar = StandardScaler()
+    df_b[['popularity', 'vote_average']] = escalar.fit_transform(df_b[['popularity', 'vote_average']])
+
+    #se definen los parametros con los que se van a entrenar al modelo, concatenando varias columnas anteriomente descritas
+    parametros_entrenamiento = pd.concat([generos_df,df_b['pertenece_colecion'],df_b['titulo_similar'],df_b['vote_average'],df_b['popularity']],axis=1)
+
+    #se entrena el modelo
+    knn.fit(parametros_entrenamiento)
+
+    # se sustraen los indices de los titulos mas cercanos al titulo ingresado
+    indices = knn.kneighbors(parametros_entrenamiento.loc[df_b['title'] == titulo])[1].flatten()
+    
+    #se hace una lista de los titulos recomendados por medio de sus indices
+    recomendaciones = list(df_b.iloc[indices]['title'])
+
+    # se organizan las recomendaciones por el valor del promedio de los votos
+    recomendaciones = sorted( recomendaciones, key= lambda x:(df_b.loc[df_b.title ==x]['titulo_similar'].values[0],df_b.loc[df_b['title'] == x]['vote_average'].values[0] ),reverse=True )
+
+    # se retira de las recomendaciones el mismo titulo ingresado en la funcion
+    recomendaciones = [pelicula for pelicula in recomendaciones if pelicula != titulo]
+
+    #se retorna la lista de recomendaciones
+    return {'lista recomendada': recomendaciones[:5]}
